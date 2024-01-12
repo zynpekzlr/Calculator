6fc557ece3a47baa74439e08cb45e5899dd9eb41
zynpekzlrt <zynpekzlr@gmail.com>
2024-01-12 18:22:31 +0300

    canculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import * as math from 'mathjs'
 import './App.css';
+import Button from './components/Button'
+import Input from './components/input'
 
 function App() {
+  const [text,setText] = useState("")
+  const [result,setResult] = useState("");
+
+  const addToText = (val) => {
+    setText((text) => [...text, val]);
+  };
+  
+
+  const calculateResult = () => {
+    try {
+      const input = text.join("");
+      setResult(math.evaluate(input));
+    } catch (error) {
+      setResult("Hata");
+    }
+  };
+  
+
+  const resetInput = ()=>{
+    setText("");
+    setResult("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='calc-wrapper'>
+        <Input text={text} result={result} />
+        <div className='row'>
+        <Button symbol="7" handleClick={addToText}/>
+        <Button symbol="8" handleClick={addToText}/>
+        <Button symbol="9" handleClick={addToText}/>
+        <Button symbol="/" color="#00AD85" handleClick={addToText}/>
+        </div>
+        <div className='row'>
+        <Button symbol="4" handleClick={addToText}/>
+        <Button symbol="5" handleClick={addToText}/>
+        <Button symbol="6" handleClick={addToText}/>
+        <Button symbol="*" color="#00AD85" handleClick={addToText}/>
+        </div>
+        <div className='row'>
+        <Button symbol="1" handleClick={addToText}/>
+        <Button symbol="2" handleClick={addToText}/>
+        <Button symbol="3" handleClick={addToText}/>
+        <Button symbol="+" color="#00AD85" handleClick={addToText}/>
+        </div>
+        <div className='row'>
+        <Button symbol="0" handleClick={addToText}/>
+        <Button symbol="." handleClick={addToText}/>
+        <Button symbol="=" handleClick={calculateResult}/>
+        <Button symbol="-" color="#00AD85" handleClick={addToText}/>
+        </div>
+        <Button symbol="Clear" color="#FF7043" handleClick={resetInput}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/input.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './input.css'
+
+function input({text,result,}) {
+  return (
+    <div className='input-wrapper'>
+        <div className='result'>
+        <h1>{result}</h1>
+    </div>
+    <div className='text'>
+        <h3>{text}</h3>
+    </div>
+</div>
+    
+  )
+}
+
+export default input
